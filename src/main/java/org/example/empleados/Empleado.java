630c90b8bc5e35d781abb0e7728825cc2af3dc3c
termisanju <psja2006@gmail.com>
2025-12-15 10:48:41 +0100

    empleados

---
+++ src/main/java/org/example/empleados/Empleado.java
@@ -0,0 +1,47 @@
+package org.example.empleados;
+
+public abstract class Empleado {
+    protected String nif;
+    protected String nombre;
+    protected String apellido;
+    protected int horasTrabajadas;
+
+    public Empleado(String nif, String nombre, String apellido, int horasTrabajadas) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+}
---
+++ src/main/java/org/example/empleados/JefeProyecto.java
@@ -0,0 +1,4 @@
+package org.example.empleados;
+
+public class JefeProyecto {
+}
